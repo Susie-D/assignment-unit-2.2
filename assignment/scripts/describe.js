43322830a5052f08de23ee06f065747e5663caf3
Susie Douang <soutsada.d@gmail.com>
2024-02-27 17:45:50 -0600

    answers 1-3

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,10 +1,9 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
-
 
 // 0. WRITE YOUR DESCRIPTION HERE
 // //EXAMPLE. We make a variable called number and set it to 1 as a number.
@@ -24,12 +23,12 @@ if (number >= 2) {
 
 */
 
-
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called name and set as a string variable with an initial value of "Dane".
+// We check if the name variable is equivalent the initial value of "Dane" with the value of "Mary".
+// If the name variable is "Mary", we console.log, 'Hi, Mary'.
+// If the name variable is not "Mary", we instead console.log 'How do you do?'.
+// In this case, since the initial value is "Dane" (and we did not update the value), we would console.log 'How do you do?' as a result.
 
 //CODE
 /*
@@ -45,10 +44,10 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called secret. We also create a variable called code. The variable code has a value which is set to 123 as a number.
+// If code has the value 123, we set and update the variable secret to a 'super' which is a string. We also set and update the variable code (which has the initial number value of 123). Code is now 123 multiped by 2 which equals to a number value of 246.
+// Now we move on the next code block to check and compare our values. If code is greater than the number value of 250, (which it is NOT since we discovered that it equals to 246), the variable secret would have been set and updated the to 'duper' string value.
+// When we console.log secret, we should have an output string value of 'super'.
 
 //CODE
 /*
@@ -70,10 +69,14 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called isStudent. The variable isStudent is set to a value of true. 
+// When a value is set to only true (or only false), we consider the variable to have a boolean value. 
+// We create a variable called age that is set to a number value of 34 that represents a student's age. We also create a variable called zip that is set to a number value of 55407 that represents an area code.
+// Now we move on to the next code block and compare the variables and the values the variables contain. 
+// If the isStudent boolean value is true, as well as the zip code number value is less than the number value 80000, console.log will output a string value 'You're a student on the West Coast!'
+// If the isStudent boolean value is false, or the age number value is less than the number value 30, console.log will output a string value 'What are your hobbies?'
+// If none of the previous statements met the statement's criteria, we would move on to the next code block and only check if the isStudent variable has a value of true. Since our initial value of isStudent is true, the initial value of zip is not greater than 8000, and the initial value of age is not less than 30, we would indeed console.log 'Welcome to Prime'. This would complete the code.
+// We would not move on to the next statement and we would not console.log 'How about the weather?'.
 
 //CODE
 /*
@@ -93,11 +96,8 @@ if (isStudent === true && zip > 80000 ) {
 
 */
 
-
-
-
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -106,7 +106,7 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
@@ -123,10 +123,8 @@ if (number >= 2) {
 
 */
 
-
-
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
@@ -140,7 +138,7 @@ if (mix === true) {
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
@@ -153,7 +151,7 @@ if (temp > 39 || time >= 4) {
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
 
 /*
@@ -166,4 +164,3 @@ if(minAge <= age) {
   console.log('enter');
 }
 */
-
